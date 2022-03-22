b8fff3cb94b190cfec86b2b9cb2a5f97fc399c26
Sodaman <mattrick2214@gmail.com>
2022-03-22 18:54:35 -0500

    wrote in the style.css for li format and added image

--- react-profile/src/components/Nav/index.js
+++ react-profile/src/components/Nav/index.js
@@ -2,42 +2,49 @@ import React from "react";
 
 function Nav() {
     const categories = [
-        {
-            name: "",
-            description: "",
-        },
-        { name: "about", description :"A description of myself and my expertise I've learned in the past year.",},
-        { name: "portfolio", description :"This is a portfolio of my works!",},
-        { name: "contact", description: "This is a page where you can contact me on the daily."},
-        { name: "resume", description: "and This is where I keep my resume! (IF I HAD ONE!)"}
+        { name: "About", description :"A description of myself and my expertise I've learned in the past year.",},
+        { name: "Portfolio", description :"This is a portfolio of my works!",},
+        { name: "Contact", description: "This is a page where you can contact me on the daily."},
+        { name: "Resume", description: "and This is where I keep my resume! (IF I HAD ONE!)"}
     ];
+
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+    }
+
     return (
-        <header>
+        <header data-testid="header" className="flex-row px-1">
             <h2>
                 <a href="/">
                 <span role="img" aria-label="camera"> 📸</span> Mattrick127's Login Page!
                 </a>
             </h2>
+
             <nav>
+
                 <ul className="flex-row">
+
                 <li className="mx-2">
-                    <a href="#about">
+                    <a href="#about" onClick={() => categorySelected()}>
                     About me
                     </a>
                 </li>
-                <li>
+{/* 
+                <li className={"mx-2"}>
                     <span>Contact</span>
-                </li>
+                </li> */}
+
                 {categories.map((category) => (
                     <li
-                    classname="mx-1"
+                    className="mx-1"
                     key={category.name}
                     >
-                        <span>
+                        <span onClick={categorySelected} >
                             {category.name}
                         </span>
                     </li>
                 ))}
+
                 </ul>
             </nav>
         </header>
