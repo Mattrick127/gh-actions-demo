58ff7a8a7f7e6e456022c48bc5efb01b760b7040
Sodaman <mattrick2214@gmail.com>
2022-03-27 22:48:03 -0500

    will work on footer to stay footer later

--- react-profile/src/components/Footer/index.js
+++ react-profile/src/components/Footer/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
     return (
-        <div>
-            Yo yo yo!
-        </div>
+        <footer>
+            YO YO YO!
+        </footer>
     )
 }
 
