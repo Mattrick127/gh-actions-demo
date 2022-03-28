8dca3f8ccfbdd1d7d45760a49599c65de90f5de8
Sodaman <mattrick2214@gmail.com>
2022-03-27 22:03:02 -0500

    was able to get contact form to open and changed the camera icon with something alot more me

--- react-profile/src/components/Nav/index.js
+++ react-profile/src/components/Nav/index.js
@@ -1,50 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function Nav() {
-    const categories = [
-    ];
+function Nav(props) {
+    const {
+      categories = [],
+      setCurrentCategory,
+      contactSelected,
+      currentCategory,
+      setContactSelected,
+    } = props;
 
-    const categorySelected = (item) => {
-        console.log(item);
-        return item;
-    };
-
+    useEffect(() => {
+      document.title = (currentCategory.name);
+    }, [currentCategory]);
+  
     return (
-        <header className="flex-row px-1">
-            <h2>
-                <a data-testid="link" href="/">
-                <span role="img" aria-label="camera"> 📸</span> Mattrick127s Page
-                </a>
-            </h2>
-
-            <nav>
-
-                <ul className="flex-row">
-
-                <li className="mx-2">
-                    <a data-testid="about" href="#about" onClick={() => categorySelected('About me')}>
-                    About me
-                    </a>
-                </li>
-
-                <li className={"mx-2"}>
-                    <span onClick={() => categorySelected('Contact')}>
-                    Contact
-                    </span>
-                </li>
-
-                {categories.map((category) => (
-                    <li className="mx-1" key={category.name}>
-                        <span onClick={() => categorySelected(category.name)}>
-                            {category.name}
-                        </span>
-                    </li>
-                ))}
-
-                </ul>
-            </nav>
-        </header>
+      <header className="flex-row px-1">
+        <h2>
+          <a data-testid="link" href="/">
+                <span role="img" aria-label="skull">&#9760;</span> Mattrick127s Page
+          </a>
+        </h2>
+        <nav>
+          <ul className="flex-row">
+            <li className="mx-2">
+              <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+                About me
+              </a>
+            </li>
+            <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+              <span onClick={() => setContactSelected(true)}>Contact</span>
+            </li>
+            {categories.map((category) => (
+              <li
+                className={`mx-1 ${
+                  currentCategory.name === category.name && !contactSelected && 'navActive'
+                  }`}
+                key={category.name}
+              >
+                <span
+                  onClick={() => {
+                    setCurrentCategory(category);
+                    setContactSelected(false);
+                  }}
+                >
+                </span>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
     );
-}
+  }
+  
 
 export default Nav;
